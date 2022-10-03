3105dc005b0e6ab034625977512b04493b94c2c4
p72losaj <p72losaj@uco.es>
2022-10-03 18:15:14 +0200

    inicio practica1

--- PRACTICAS/PRACTICA1/KNN.py
+++ PRACTICAS/PRACTICA1/KNN.py
@@ -4,18 +4,20 @@ import pandas as pd
 from sklearn import datasets
 from sklearn import metrics 
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
+
 # variables
-arbol_size = []; arbol_accuracy = []; arbol_error=[]
+knn_size = []; knn_accuracy = []; knn_error=[]
+
 # Cargamos el dataset Iris.data
 dataset = datasets.load_iris()
 # Dividimos el dataset en train y test
 X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.2, random_state=1)
 # Tamano del modelo
-arbol_size.append(len(X_train))
+knn_size.append(len(X_train))
 # Creamos el modelo
-model = DecisionTreeClassifier()
+model = KNeighborsClassifier()
 # Entrenamos el modelo
 model.fit(X_train, y_train)
 # Hacemos las predicciones
@@ -25,17 +27,17 @@ precision = metrics.accuracy_score(y_test, y_pred)
 # Calculamos el error
 error = 1 - precision
 # Guardamos los resultados
-arbol_accuracy.append(precision)
-arbol_error.append(error)
+knn_accuracy.append(precision)
+knn_error.append(error)
 
 # Cargamos el dataset Wine data
 wine = datasets.load_wine()
 # Dividimos el dataset en train y test
 X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target, test_size=0.2, random_state=1)
 # Tamano del modelo
-arbol_size.append(len(X_train))
+knn_size.append(len(X_train))
 # Creamos el modelo
-model = DecisionTreeClassifier()
+model = KNeighborsClassifier()
 # Entrenamos el modelo
 model.fit(X_train, y_train)
 # Hacemos las predicciones
@@ -45,17 +47,17 @@ precision = metrics.accuracy_score(y_test, y_pred)
 # Calculamos el error
 error = 1 - precision
 # Guardamos los resultados
-arbol_accuracy.append(precision)
-arbol_error.append(error)
+knn_accuracy.append(precision)
+knn_error.append(error)
 
 # Cargamos el dataset Diabetes.data
 diabetes = datasets.load_diabetes()
 # Dividimos el dataset en train y test
 X_train, X_test, y_train, y_test = train_test_split(diabetes.data, diabetes.target, test_size=0.2, random_state=1)
 # Tamano del modelo
-arbol_size.append(len(X_train))
+knn_size.append(len(X_train))
 # Creamos el modelo
-model = DecisionTreeClassifier()
+model = KNeighborsClassifier()
 # Entrenamos el modelo
 model.fit(X_train, y_train)
 # Hacemos las predicciones
@@ -65,17 +67,17 @@ precision = metrics.accuracy_score(y_test, y_pred)
 # Calculamos el error
 error = 1 - precision
 # Guardamos los resultados
-arbol_accuracy.append(precision)
-arbol_error.append(error)
+knn_accuracy.append(precision)
+knn_error.append(error)
 
 # Cargamos el dataset Breast Cancer.data
-cancer = datasets.load_breast_cancer()
+breast_cancer = datasets.load_breast_cancer()
 # Dividimos el dataset en train y test
-X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(breast_cancer.data, breast_cancer.target, test_size=0.2, random_state=1)
 # Tamano del modelo
-arbol_size.append(len(X_train))
+knn_size.append(len(X_train))
 # Creamos el modelo
-model = DecisionTreeClassifier()
+model = KNeighborsClassifier()
 # Entrenamos el modelo
 model.fit(X_train, y_train)
 # Hacemos las predicciones
@@ -85,18 +87,17 @@ precision = metrics.accuracy_score(y_test, y_pred)
 # Calculamos el error
 error = 1 - precision
 # Guardamos los resultados
-arbol_accuracy.append(precision)
-arbol_error.append(error)
+knn_accuracy.append(precision)
+knn_error.append(error)
 
 
 
 
-# Mostramos el arbol
-plt.plot(arbol_size, arbol_accuracy, label='Accuracy')
-plt.plot(arbol_size, arbol_error, label='Error')
+# Mostramos el algoritmo KNN
+plt.plot(knn_size, knn_accuracy, label='Accuracy')
+plt.plot(knn_size, knn_error, label='Error')
 plt.xlabel('Tamaño del modelo')
 plt.ylabel('Accuracy/Error')
-plt.title('Arbol de decision')
+plt.title('KNN')
 plt.legend()
-plt.savefig("images/ArbolDecision.png")
-
+plt.savefig("images/ejercicio2/K-vecinos.png")
