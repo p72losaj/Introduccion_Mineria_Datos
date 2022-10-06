7fd737dc5576681d09c128265cbd9bc319c2f976
p72losaj <p72losaj@uco.es>
2022-10-06 18:05:45 +0200

    Ejercicio 4 para 3 dimensiones

--- PRACTICAS/PRACTICA1/ejercicio4/3dimensiones.py
+++ PRACTICAS/PRACTICA1/ejercicio4/3dimensiones.py
@@ -7,12 +7,12 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-import matplotlib.pyplot as plt # Grafica PCA 2D    
+import matplotlib.pyplot as plt # Grafica PCA 3D
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
-# variables para el grafico de PCA de 2 dimensiones
+# variables para el grafico de PCA de 3 dimensiones
 knn_size = []; knn_accuracy = []; knn_error=[]
 arbol_size = []; arbol_accuracy = []; arbol_error=[]
 
@@ -30,11 +30,11 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Apply PCA 2D
-pca = PCA(n_components=2)
+# Apply PCA 3D
+pca = PCA(n_components=3)
 pca.fit(X_train)
 
-# Fit and transform data 2D
+# Fit and transform data 3D
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 
@@ -42,30 +42,30 @@ X_test = pca.transform(X_test)
 model = KNeighborsClassifier()
 model2 = DecisionTreeClassifier()
 
-# Tamano del modelo 2D
+# Tamano del modelo 3D
 knn_size.append(len(X_train))
 arbol_size.append(len(X_train))
 
 
-# Entrenamos el modelo 2D
+# Entrenamos el modelo 3D
 model.fit(X_train, y_train)
 model2.fit(X_train, y_train)
 
 
-# Hacemos las predicciones 2D
+# Hacemos las predicciones 3D
 y_pred = model.predict(X_test)
 y_pred2 = model2.predict(X_test)
 
 
-# Calculamos la precision 2D
+# Calculamos la precision 3D
 precision = metrics.accuracy_score(y_test, y_pred)
 precision2 = metrics.accuracy_score(y_test, y_pred2)
 
-# Calculamos el error 2D
+# Calculamos el error 3D
 error = 1 - precision
 error2 = 1 - precision2
 
-# Guardamos los resultados 2D
+# Guardamos los resultados 3D
 knn_accuracy.append(precision)
 knn_error.append(error)
 arbol_accuracy.append(precision2)
@@ -84,22 +84,22 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Apply PCA 2D
+# Apply PCA 3D
 pca.fit(X_train)
 
-# Fit and transform data 2D
+# Fit and transform data 3D
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 
-# Tamano del modelo 2D
+# Tamano del modelo 3D
 knn_size.append(len(X_train))
 arbol_size.append(len(X_train))
 
-# Entrenamos el modelo 2D
+# Entrenamos el modelo 3D
 model.fit(X_train, y_train)
 model2.fit(X_train, y_train)
 
-# Hacemos las predicciones 2D
+# Hacemos las predicciones 3D
 y_pred = model.predict(X_test)
 y_pred2 = model2.predict(X_test)
 
@@ -218,6 +218,6 @@ plt.plot(knn_size, knn_error, label='KNN Error')
 plt.plot(arbol_size, arbol_error, label='Arbol de decision Error')
 plt.xlabel('Tamano del modelo (train) ')
 plt.ylabel('Precision/Error del modelo')
-plt.title('Algoritmos KNN y Arbol de decision en 2D')
+plt.title('Algoritmos KNN y Arbol de decision en 3D')
 plt.legend()
-plt.savefig('images/ejercicio4_2dimesiones.png')
+plt.savefig('images/ejercicio4_3dimesiones.png')
