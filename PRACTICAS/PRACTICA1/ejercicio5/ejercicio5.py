ff58e4a78fcef931405633f91486cf3c5a5ac727
p72losaj <p72losaj@uco.es>
2022-10-08 10:34:03 +0200

    Finalizacion ejercicio5

--- PRACTICAS/PRACTICA1/ejercicio5/ejercicio5.py
+++ PRACTICAS/PRACTICA1/ejercicio5/ejercicio5.py
@@ -125,4 +125,95 @@ plt.title('Muestreo aleatorio sin reemplazamiento del 10% ')
 plt.legend()
 plt.savefig("images/Comparacion.png")
 
+# Limpiamos las listas
+knn_size.clear(); knn_accuracy.clear(); knn_error.clear()
+arbol_size.clear(); arbol_accuracy.clear(); arbol_error.clear()
+
+# Limpiamos los datos de generacion del grafico
+plt.clf() # Clear the figure
+
+# Split data into training and test sets 10% instancias con muestreo estratificado
+X_train_iris, X_test_iris, y_train_iris, y_test_iris = train_test_split(X_iris, y_iris, test_size=0.1, random_state=0, stratify=y_iris)
+X_train_wine, X_test_wine, y_train_wine, y_test_wine = train_test_split(X_wine, y_wine, test_size=0.1, random_state=0, stratify=y_wine)
+# X_train_diabetes, X_test_diabetes, y_train_diabetes, y_test_diabetes = train_test_split(X_diabetes, y_diabetes, test_size=0.33, random_state=32, stratify=y_diabetes)
+X_train_breast_cancer, X_test_breast_cancer, y_train_breast_cancer, y_test_breast_cancer = train_test_split(X_breast_cancer, y_breast_cancer, test_size=0.1, random_state=0, stratify=y_breast_cancer)
+
+# Tamano de los datos de entrenamiento
+knn_size.append(len(X_train_iris))
+knn_size.append(len(X_train_wine))
+# knn_size.append(len(X_train_diabetes))
+knn_size.append(len(X_train_breast_cancer))
+arbol_size.append(len(X_train_iris))
+arbol_size.append(len(X_train_wine))
+# arbol_size.append(len(X_train_diabetes))
+arbol_size.append(len(X_train_breast_cancer))
+
+
+# Entrenamos el modelo iris
+model.fit(X_train_iris, y_train_iris)
+model2.fit(X_train_iris, y_train_iris)
+
+# Hacemos las predicciones iris
+y_pred_iris = model.predict(X_test_iris)
+y_pred2_iris = model2.predict(X_test_iris)
+
+# Calculamos la precision iris
+knn_accuracy.append(metrics.accuracy_score(y_test_iris, y_pred_iris))
+arbol_accuracy.append(metrics.accuracy_score(y_test_iris, y_pred2_iris))
+
+# Calculamos el error iris
+knn_error.append(1-metrics.accuracy_score(y_test_iris, y_pred_iris))
+arbol_error.append(1-metrics.accuracy_score(y_test_iris, y_pred2_iris))
+
+# Entrenamos el modelo wine
+model.fit(X_train_wine, y_train_wine)
+model2.fit(X_train_wine, y_train_wine)
+
+# Hacemos las predicciones wine
+y_pred_wine = model.predict(X_test_wine)
+y_pred2_wine = model2.predict(X_test_wine)
+
+# Calculamos la precision wine
+knn_accuracy.append(metrics.accuracy_score(y_test_wine, y_pred_wine))
+arbol_accuracy.append(metrics.accuracy_score(y_test_wine, y_pred2_wine))
+
+# Calculamos el error wine
+knn_error.append(1-metrics.accuracy_score(y_test_wine, y_pred_wine))
+arbol_error.append(1-metrics.accuracy_score(y_test_wine, y_pred2_wine))
+
+# Entrenamos el modelo diabetes
+
+# Hacemos las predicciones diabetes
+
+# Calculamos la precision diabetes
+
+# Calculamos el error diabetes
+
+# Entrenamos el modelo breast cancer
+model.fit(X_train_breast_cancer, y_train_breast_cancer)
+model2.fit(X_train_breast_cancer, y_train_breast_cancer)
+
+# Hacemos las predicciones breast cancer
+y_pred_breast_cancer = model.predict(X_test_breast_cancer)
+y_pred2_breast_cancer = model2.predict(X_test_breast_cancer)
+
+# Calculamos la precision breast cancer
+knn_accuracy.append(metrics.accuracy_score(y_test_breast_cancer, y_pred_breast_cancer))
+arbol_accuracy.append(metrics.accuracy_score(y_test_breast_cancer, y_pred2_breast_cancer))
+
+# Calculamos el error breast cancer
+knn_error.append(1-metrics.accuracy_score(y_test_breast_cancer, y_pred_breast_cancer))
+arbol_error.append(1-metrics.accuracy_score(y_test_breast_cancer, y_pred2_breast_cancer))
+
+# Generamos el grafico de accuracy y error
+plt.plot(knn_size, knn_accuracy, label='KNN_Accuracy')
+plt.plot(knn_size, knn_error, label='KNN_Error')
+plt.plot(arbol_size, arbol_accuracy, label='Arbol_Accuracy')
+plt.plot(arbol_size, arbol_error, label='Arbol_Error')
+plt.xlabel('Tamaño del modelo (train)')
+plt.ylabel('Accuracy/Error')
+plt.title('Muestreo estratificado del 10% ')
+plt.legend()
+plt.savefig("images/MuestreoEstratificado.png")
+plt.show()
 
