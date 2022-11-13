b1857c3ca34d1aeb77a9bd81ae9f3e69633f398a
p72losaj <p72losaj@uco.es>
2022-11-13 14:27:55 +0100

    Continuacion ejercicio9

--- PRACTICAS/PRACTICA2/ejercicio9.py
+++ PRACTICAS/PRACTICA2/ejercicio9.py
@@ -1,7 +1,5 @@
-# Ejercicio 9 de la practica 2 de IMD
+# Ejercicio 8 de la practica 2 de imd
 # Autor: Jaime Lorenzo Sanchez
-# Aplicar el metodo RIPPER del modulo wiitgenstein en al menos 3 conjuntos de datos
-# de 2 clases diferentes. Evaluar los modelos obtenidos y compararlos con el arbol de decision
 
 # Listado de bibliotecas
 from sklearn import datasets
@@ -9,31 +7,18 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 import wittgenstein as lw
+import pandas as pd
 
-# Dataset a entrenar y testear
-iris = datasets.load_iris()
-wine = datasets.load_wine()
-breast_cancer = datasets.load_breast_cancer()
-
-# Modelos de entrenamiento
-ripper = lw.RIPPER()
-arbol = DecisionTreeClassifier()
-pred_ripper = []; precision_ripper = []
-pred_arbol = []; precision_arbol = []
-
-# Entrenamos el arbol de decision
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=1)
-arbol.fit(X_train, y_train)
-pred_arbol = arbol.predict(X_test)
-precision_arbol.append(metrics.accuracy_score(y_test, pred_arbol))
-
-X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target, test_size=0.3, random_state=1)
-arbol.fit(X_train, y_train)
-pred_arbol = arbol.predict(X_test)
-precision_arbol.append(metrics.accuracy_score(y_test, pred_arbol))
-
-X_train, X_test, y_train, y_test = train_test_split(breast_cancer.data, breast_cancer.target, test_size=0.3, random_state=1)
-arbol.fit(X_train, y_train)
-pred_arbol = arbol.predict(X_test)
-precision_arbol.append(metrics.accuracy_score(y_test, pred_arbol))
-
+# Read dataset
+df = pd.read_csv("iris.csv")
+train, test = train_test_split(df, test_size=.33)
+# Create and train model
+ripper_clf = lw.RIPPER()
+ripper_precision = []
+ripper_clf.fit(df, class_feat="variety", pos_class='Versicolor')
+# Score
+X_test = test.drop('variety', axis=1)
+y_test = test['variety']
+ripper_clf.score(X_test, y_test)
+# Calculamos la precision del modelo
+ripper_precision.append(ripper_clf.score(X_test, y_test))
